3dd4ecfc443a3566a2392be4dedccd4b775f1848
pramodbontha <pramodkumar.bontha@gmail.com>
2021-05-31 19:40:55 +0200

    Implementation with react-typescript

--- react-ts-frontend/src/components/Drivers/Drivers.tsx
+++ react-ts-frontend/src/components/Drivers/Drivers.tsx
@@ -3,8 +3,9 @@ import Container from "@material-ui/core/Container";
 import Driver from "./Driver/Driver";
 import Typography from "@material-ui/core/Typography";
 import { getDriversService } from "../../services/drivers/drivers";
+import { DriverModel } from "../../models/driver.model";
 
-const Drivers = () => {
+const Drivers:React.FC<{}> = () => {
 	const [drivers, setDrivers] = useState([]);
 	const getDriverDetails = async () => {
 		try {
@@ -22,8 +23,8 @@ const Drivers = () => {
 	return (
 		<Container>
 			<Typography variant="h3">Drivers</Typography>
-			{drivers.map((driver, index) => (
-				<Driver key={driver + index} driver={driver} />
+			{drivers.map((driver: DriverModel, index: number) => (
+				<Driver key={index} driver={driver} />
 			))}
 		</Container>
 	);
